d9dd19685a3d21d268c70acd7a4cda949cf8e6de
HP <!@mail.ru>
2023-11-08 21:27:46 +0300

    Add metods:        printThreeWords();                    checkSumSign();                    printColor();                    compareNumbers();

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
 public class Main {
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
 
-        System.out.println("asdfghjkjhgfdf");
+
     }
+
+    private static void compareNumbers() {
+        int a = 3;
+        int b = 5;
+        if (a >= b){
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+
+    private static void printColor() {
+        int value = 156;
+        if (value <=0){
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100){
+            System.out.println("Желтый");
+        } else if (value > 100){
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void checkSumSign() {
+        int a = 5;
+        int b =7;
+        int g = a + b;
+        if (g >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+
+
+    }
+
+
 }
