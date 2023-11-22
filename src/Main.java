7bc2666c8931d00418ce77586205ca2ba6fc42e8
MeriVei <glushhenko1910@mail.ru>
2023-11-22 19:12:54 +0300

    Первае задание

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,26 @@ public class Main {
         Cat[] cats = new Cat[5];
         cats[0] = new Cat("Кот 1", 20);
         cats[1] = new Cat("Кот 2", 5);
-        cats[2] = new Cat("Кот 3", 30);
-        cats[3] = new Cat("Кот 4", 10);
+        cats[2] = new Cat("Кот 3", 10);
+        cats[3] = new Cat("Кот 4", 30);
         cats[4] = new Cat("Кот 5", 7);
-        Plate plate = new Plate(70);
+        Plate plate = new Plate(60);
+        boolean isNotSatiety = false;
+        do {
+            isNotSatiety = false;
+            for (Cat c:cats) {
+                c.setSatiety(plate.eat(c.getAppetite()));
+                System.out.println(c.getName()+" "+(c.isSatiety() ? "сыт" : "голоден"));
+                if (!c.isSatiety()){
+                    isNotSatiety = true;
+                }
+            }
+            if (plate.getFood() < 50) {
+                plate.addFood(100);
+            }
+            System.out.println();
+        } while (isNotSatiety);
 
-        for (Cat c:cats) {
-            c.setSatiety(plate.eat(c.getAppetite()));
-            System.out.println(c.getName()+" "+(c.isSatiety() ? "сыт" : "голоден"));
-        }
+
     }
 }
