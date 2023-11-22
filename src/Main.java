5507a49a60f95fddf66e6a7c93c8350c456f3c0e
MeriVei <glushhenko1910@mail.ru>
2023-11-22 18:00:50 +0300

    Тест имени профиля

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
         Employee[] a = new Employee[5];
         a[0] = new Employee("Хомяков Т.Д", "Инженер", "chfhbd898@mail.ru", 698587, 90003, 33);
-        a[1] = new Employee("Пирожков Д.С", "Инженер", "hfvgb8@mail.ru", 5385853, 10005, 29);
+        a[1] = new Employee("Пирожков Д.С", "Слесарь", "hfvgb8@mail.ru", 5385853, 10005, 29);
         a[2] = new Employee("Горохов А.А", "Инженер", "klklkl526@mail.ru", 21561651, 3215, 22);
-        a[3] = new Employee("Росков П.Р", "Инженер", "hffv555@mail.ru", 654646845, 15454, 45);
+        a[3] = new Employee("Росков П.Р", "Техник", "hffv555@mail.ru", 654646845, 15454, 45);
         a[4] = new Employee("Терень Г.Л", "Инженер", "rggt5454@mail.ru", 65598458, 98543, 43);
 
         for (int i = 0; i < a.length; i++) {
