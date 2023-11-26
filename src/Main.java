d605d32253e0cc76cdca892b584aa1fa95ff8ed0
MeriVei <glushhenko1910@mail.ru>
2023-11-26 22:57:59 +0300

    Начало

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("asdfghjkjhgfdf");
+        String[] a = {"Hell","Hi","Hi","Hello","By","By","By","Meri","By","Hi","Hello","By","Hi"};
+        HashSet<String> hashSet = new HashSet<>();
+        for (String data : a){
+            hashSet.add(data);
+        }
+
+        for (String data : hashSet){
+            System.out.println(data);
+        }
+
     }
 }
