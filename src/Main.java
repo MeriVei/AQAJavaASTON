0ba04eb6749ed01d663a08d485881f3f3d9be9c4
MeriVei <glushhenko1910@mail.ru>
2023-11-27 22:46:00 +0300

    Начало

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("asdfghjkjhgfdf");
+        try {
+            sum(new String[][]{});
+        } catch (MyArraySizeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sum(String[][] aq) throws MyArraySizeException {
+        if (aq.length !=4){
+            throw new MyArraySizeException("Размер массива не равен 4х4");
+        }
+        for (int i = 0; i < aq.length; i++) {
+            if (aq[i].length!=4){
+                throw new MyArraySizeException("Размер массива не равен 4х4");
+            }
+        }
     }
 }
