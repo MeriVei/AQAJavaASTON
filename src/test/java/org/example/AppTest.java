8c2f4c0204ed1b97e61f48cdd5035adfa811d178
MeriVei <glushhenko1910@mail.ru>
2023-12-24 19:19:46 +0300

    Финиш

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,11 +3,13 @@ package org.example;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AppTest {
@@ -20,9 +22,9 @@ public class AppTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         onlinePay = new OnlinePay(driver);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://www.mts.by");
         driver.findElement(By.id("cookie-agree")).click();
     }
@@ -35,13 +37,13 @@ public class AppTest {
     @Order(1)
     @Test
     public void nameTest() {
-        Assertions.assertTrue(onlinePay.isName("Онлайн пополнение\nбез комиссии"));
+        Assertions.assertTrue(onlinePay.equalsName("Онлайн пополнение\nбез комиссии"));
     }
 
     @Order(2)
     @Test
     public void payPartnersTest() {
-        Assertions.assertTrue(onlinePay.isPayPartners());
+        Assertions.assertTrue(onlinePay.isEmptyPayPartners());
     }
 
     @Order(3)
@@ -55,19 +57,70 @@ public class AppTest {
 
     @Order(4)
     @Test
+    public void titlesTest() {
+        Assertions.assertEquals(onlinePay.getConnectionPhonePlaceholder(),
+                "Номер телефона");
+        Assertions.assertEquals(onlinePay.getConnectionSumPlaceholder(),
+                "Сумма");
+        Assertions.assertEquals(onlinePay.getConnectionEmailPlaceholder(),
+                "E-mail для отправки чека");
+        Assertions.assertEquals(onlinePay.getInternetPhonePlaceholder(),
+                "Номер абонента");
+        Assertions.assertEquals(onlinePay.getInternetSumPlaceholder(),
+                "Сумма");
+        Assertions.assertEquals(onlinePay.getInternetEmailPlaceholder(),
+                "E-mail для отправки чека");
+        Assertions.assertEquals(onlinePay.getScoreInstalmentPlaceholder(),
+                "Номер счета на 44");
+        Assertions.assertEquals(onlinePay.getInstalmentSumPlaceholder(),
+                "Сумма");
+        Assertions.assertEquals(onlinePay.getInstalmentEmailPlaceholder(),
+                "E-mail для отправки чека");
+        Assertions.assertEquals(onlinePay.getScoreArrearsPlaceholder(),
+                "Номер счета на 2073");
+        Assertions.assertEquals(onlinePay.getArrearsSumPlaceholder(),
+                "Сумма");
+        Assertions.assertEquals(onlinePay.getArrearsEmailPlaceholder(),
+                "E-mail для отправки чека");
+    }
+
+    @Order(5)
+    @Test
     public void clickBtnTest() {
+        String money = "10.50";
+        String phone = "297777777";
         onlinePay.clickButtonServices();
-        onlinePay.clickButtonServiceOne();
-        onlinePay.inputPhone("297777777");
-        onlinePay.inputMoney("10");
+        onlinePay.clickButtonCommunicationServices();
+        onlinePay.inputPhone(phone);
+        onlinePay.inputMoney(money);
         onlinePay.inputEmail("test@gmail.com");
         onlinePay.clickButtonContinue();
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//iframe[@class = 'bepaid-iframe']")));
         driver.switchTo().frame(driver.findElement(
                 By.xpath("//iframe[@class = 'bepaid-iframe']")));
         Assertions.assertEquals("BePaidWidget",
                 driver.findElement(By.tagName("title")).getAttribute("innerHTML"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//p[@class='header__payment-amount']")));
+        Assertions.assertEquals(money + " BYN", driver.findElement(
+                By.xpath("//p[@class='header__payment-amount']")).getText());
+        Assertions.assertEquals("Оплатить " + money + " BYN", driver.findElement(
+                By.xpath("//button[@class='colored disabled ng-star-inserted']")).getText());
+        Assertions.assertEquals("Оплата: Услуги связи Номер:375" + phone, driver.findElement(
+                By.xpath("//p[@class='header__payment-info']")).getText());
+        Assertions.assertEquals("Номер карты", driver.findElement(
+                By.xpath("//input[@formcontrolname='creditCard']/parent::*/label")).getText());
+        Assertions.assertEquals("Срок действия", driver.findElement(
+                By.xpath("//input[@formcontrolname='expirationDate']/parent::*/label")).getText());
+        Assertions.assertEquals("CVC", driver.findElement(
+                By.xpath("//input[@formcontrolname='cvc']/parent::*/label")).getText());
+        Assertions.assertEquals("Имя держателя (как на карте)", driver.findElement(
+                By.xpath("//input[@formcontrolname='holder']/parent::*/label")).getText());
+        List<WebElement> iconsPay = driver.findElements(
+                By.xpath("//input[@formcontrolname='creditCard']" +
+                        "/parent::*/parent::*/div/div/div/child::*"));
+        Assertions.assertFalse(iconsPay.isEmpty());
         driver.switchTo().parentFrame();
     }
 }
--- src/test/java/org/example/OnlinePay.java
+++ src/test/java/org/example/OnlinePay.java
@@ -20,25 +20,55 @@ public class OnlinePay {
     @FindBy(xpath = "//section[@class = 'pay']//button[@class = 'select__header']")
     private WebElement buttonServices;
     @FindBy(xpath = "//section[@class = 'pay']//ul[@class = 'select__list']/li[1]")
-    private WebElement buttonServiceOne;
+    private WebElement buttonCommunicationServices;
+    @FindBy(xpath = "//section[@class = 'pay']//ul[@class = 'select__list']/li[2]")
+    private WebElement buttonHomeInternet;
+    @FindBy(xpath = "//section[@class = 'pay']//ul[@class = 'select__list']/li[3]")
+    private WebElement buttonInstallmentPlan;
+    @FindBy(xpath = "//section[@class = 'pay']//ul[@class = 'select__list']/li[4]")
+    private WebElement buttonDebt;
     @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'connection-phone']")
-    private WebElement phone;
+    private WebElement connectionPhone;
     @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'connection-sum']")
-    private WebElement money;
+    private WebElement connectionSum;
     @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'connection-email']")
-    private WebElement email;
+    private WebElement connectionEmail;
     @FindBy(xpath = "//section[@class = 'pay']//form[@id = 'pay-connection']/button")
-    private WebElement buttonContinue;
+    private WebElement buttonPayConnection;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'internet-phone']")
+    private WebElement internetPhone;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'internet-sum']")
+    private WebElement internetSum;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'internet-email']")
+    private WebElement internetEmail;
+    @FindBy(xpath = "//section[@class = 'pay']//form[@id = 'pay-internet']/button")
+    private WebElement buttonPayInternet;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'score-instalment']")
+    private WebElement scoreInstalment;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'instalment-sum']")
+    private WebElement instalmentSum;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'instalment-email']")
+    private WebElement instalmentEmail;
+    @FindBy(xpath = "//section[@class = 'pay']//form[@id = 'pay-instalment']/button")
+    private WebElement buttonPayInstalment;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'score-arrears']")
+    private WebElement scoreArrears;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'arrears-sum']")
+    private WebElement arrearsSum;
+    @FindBy(xpath = "//section[@class = 'pay']//input[@id = 'arrears-email']")
+    private WebElement arrearsEmail;
+    @FindBy(xpath = "//section[@class = 'pay']//form[@id = 'pay-arrears']/button")
+    private WebElement buttonPayArrears;
     public OnlinePay(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
-    public boolean isName(String str) {
+    public boolean equalsName(String str) {
         return name.getText().equals(str);
     }
 
-    public boolean isPayPartners() {
+    public boolean isEmptyPayPartners() {
         List<String> webElements = payPartners.findElements(By.tagName("img"))
                 .stream().map(p -> p.getAttribute("src")).collect(Collectors.toList());
         return !webElements.isEmpty();
@@ -52,23 +82,87 @@ public class OnlinePay {
         buttonServices.click();
     }
 
-    public void clickButtonServiceOne() {
-        buttonServiceOne.click();
+    public void clickButtonCommunicationServices() {
+        buttonCommunicationServices.click();
+    }
+
+    public void clickButtonHomeInternet() {
+        buttonHomeInternet.click();
+    }
+
+    public void clickButtonInstallmentPlan() {
+        buttonInstallmentPlan.click();
+    }
+
+    public void clickButtonDebt() {
+        buttonDebt.click();
     }
 
     public void inputPhone(String str) {
-        phone.sendKeys(str);
+        connectionPhone.sendKeys(str);
     }
 
     public void inputMoney(String str) {
-        money.sendKeys(str);
+        connectionSum.sendKeys(str);
     }
 
     public void inputEmail(String str) {
-        email.sendKeys(str);
+        connectionEmail.sendKeys(str);
     }
 
     public void clickButtonContinue() {
-        buttonContinue.click();
+        buttonPayConnection.click();
+    }
+
+    public String getPlaceholder(WebElement element) {
+        return element.getAttribute("placeholder");
+    }
+
+    public String getConnectionPhonePlaceholder() {
+        return getPlaceholder(connectionPhone);
+    }
+
+    public String getConnectionSumPlaceholder() {
+        return getPlaceholder(connectionSum);
+    }
+
+    public String getConnectionEmailPlaceholder() {
+        return getPlaceholder(connectionEmail);
+    }
+
+    public String getInternetPhonePlaceholder() {
+        return getPlaceholder(internetPhone);
+    }
+
+    public String getInternetSumPlaceholder() {
+        return getPlaceholder(internetSum);
+    }
+
+    public String getInternetEmailPlaceholder() {
+        return getPlaceholder(internetEmail);
+    }
+
+    public String getScoreInstalmentPlaceholder() {
+        return getPlaceholder(scoreInstalment);
+    }
+
+    public String getInstalmentSumPlaceholder() {
+        return getPlaceholder(instalmentSum);
+    }
+
+    public String getInstalmentEmailPlaceholder() {
+        return getPlaceholder(instalmentEmail);
+    }
+
+    public String getScoreArrearsPlaceholder() {
+        return getPlaceholder(scoreArrears);
+    }
+
+    public String getArrearsSumPlaceholder() {
+        return getPlaceholder(arrearsSum);
+    }
+
+    public String getArrearsEmailPlaceholder() {
+        return getPlaceholder(arrearsEmail);
     }
 }
